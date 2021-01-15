000392feb58db3f0047741567ccba9cdad12c2b3
pak11273 <pak11273@gmail.com>
2021-01-15 12:41:32 -0600

    completes mvp

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,21 +1,36 @@
 import React, {useState} from 'react'
 
+import Form from './components/Form'
+
 function App() {
   const initialState = [
-    {name: "Brad" },
-    {name: "Brad" },
-    {name: "Brad" },
-    {name: "Brad" },
+    {name: "Stephanie Enciso", email: "stephanie-enciso@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Taylor Friesen", email: "taylor-friesen@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Gia Bleu", email: "gia-bleu@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Kaitlyn Hunnicutt", email: "kaitlyn-hunnicutt@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Kaitlyn Hunnicutt", email: "kaitlyn-hunnicutt@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Nicholas Cabuno", email: "nicholas-cabuno@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Joe Park", email: "jong-seo-park@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Mona Hassan", email: "mona-hassan@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Hairo Garcia", email: "hairo-garcia@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Devon Brady", email: "devon-brady@lambdastudents.com", role: "full-stack engineer" },
+    {name: "Robert Houck", email: "robert-houck@lambdastudents.com", role: "full-stack engineer" },
   ]
+
   const [team, setTeam] = useState(initialState)
   return (
+    <>
+    <div>
+      <Form />
+    </div>
     <div>
       <ul>
-        {team.map( member => {
-          return <li>{member.name}</li>
+        {team.map( (member,i) => {
+          return <li key={i}>{member.name}</li>
         })}
       </ul>
     </div>
+    </>
   );
 }
 
