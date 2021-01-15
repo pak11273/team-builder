a5f1a9fc495f0636ffdf4ee31e8356ba1c014a13
pak11273 <pak11273@gmail.com>
2021-01-14 18:39:34 -0600

    init

---
+++ team-builder/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>Brad</li>
+        <li>Shad</li>
+        <li>Berry</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
